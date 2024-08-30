d86c9084610d696044be26427d2891d296a7abde
nilesh7272 <nileshparadva97@gmail.com>
2024-08-30 10:53:17 +0530

    c practical exam

--- task-3.c
+++ task-3.c
@@ -4,17 +4,17 @@
 void main(){
     
    char string[20],temp;
-   int i,length;
+   int length;
 
    printf("Enter String : ");
    scanf("%s",string);
 
-   length=strlen(string)-1;
+   length = strlen(string)-1;
 
-   for(i=0;i<strlen(string)/2;i++){
-      temp=string[i];
-      string[i]=string[length];
-      string[length--]=temp;
+   for(int i=0; i < strlen(string)/2; i++){
+      temp = string[i];
+      string[i] = string[length];
+      string[length--] = temp;
    }
 
    printf("Reverse string :%s",string);
